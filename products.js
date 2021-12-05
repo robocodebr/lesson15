7fd233dcf04bfafee415fc01de5752d1bb89b694
robocodebr <robocodebr@gmail.com>
2021-12-05 20:37:54 +0300

    ыфму

---
+++ products.js
@@ -0,0 +1,11 @@
+
+
+let productsElement = document.getElementById("products");
+
+fetch("http://my-json-server.typicode.com/RobocodeSchool/marketplace/db")
+.then(function(data){
+    return data.json()
+})
+.then(function(data){
+    
+})
