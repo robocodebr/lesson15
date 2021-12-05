90658b67a11190267d3587efe710af664683dfcf
robocodebr <robocodebr@gmail.com>
2021-12-05 20:39:12 +0300

    save

---
+++ products.js
@@ -0,0 +1,12 @@
+
+
+
+let productsElement = document.getElementById("products");
+
+fetch("http://my-json-server.typicode.com/RobocodeSchool/marketplace/db")
+.then(function(data){
+    return data.json()
+})
+.then(function(data){
+    
+})
