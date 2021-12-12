1fa106c225dd11d60584dbdc9fef6e35a5753b00
robocodebr <robocodebr@gmail.com>
2021-12-12 19:12:16 +0300

    lesson 16

--- products.js
+++ products.js
@@ -5,5 +5,18 @@ fetch("http://my-json-server.typicode.com/RobocodeSchool/marketplace/db")
     return data.json()
 })
 .then(function(data){
-    
+    data.products.forEach(element => {
+        productsElement.innerHTML += addCard(element);
+    });
 })
+function addCard(card){
+return `<div class="card">
+<p class="name">${card.name}</p>
+<img src="${card.photo_url}" alt="${card.name}">
+<p class="price"><b>Price</b>:${card.price}<$</p>
+<p class="description"><b>Description</b>:${card.description}</p>
+<a href="" class="seller">Seller profile</a>
+<button type="button" class="buy">Buy</button>
+</div>`
+
+}
