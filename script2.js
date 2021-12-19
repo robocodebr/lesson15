70132fe672012f5c233004c183395c1f76c479bc
robocodebr <robocedebr@gmail.com>
2021-12-19 20:49:04 +0300

    ass buy function

--- script2.js
+++ script2.js
@@ -13,12 +13,30 @@ fetch('http://my-json-server.typicode.com/RobocodeSchool/marketplace/db')
             <p class="product_text"><span class="product_text_bold">Price: </span>${json.products[i].price}</p>
             <p class="product_text"><span class="product_text_bold">Description: </span>${json.products[i].description}</p>
             <a href="#" class="product_link">Seller profile</a>
-            <input type="submit" class="submit" value="Buy">
+            <input type="submit" class="submit" value="Buy" onclick="buy(${i})">
         </div>
         `
     }
 })
 
+function buy(i) {
+    let currentProduct = document.getElementsByClassName("products_item_container")[i];
+    let product = {}
+    product.title = currentProduct.children[0].innerHTML;
+    product.img = currentProduct.children[1].src;
+    product.price = currentProduct.children[2].childNodes[1].nodeValue;
+    let productsInCart = localStorage.getItem("cart");
+    if(localStorage.length == 0) {
+        productsInCart = []
+    } else {
+        console.log(productsInCart)
+        productsInCart = JSON.parse(productsInCart);
+    }
+
+    productsInCart.push(product)
+    localStorage.setItem("cart", JSON.stringify(productsInCart));
+    console.log(localStorage.getItem("cart"))
+}
 
 
 let cartBtn = document.getElementById("cart");
