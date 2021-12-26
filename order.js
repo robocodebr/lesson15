cf98c87c70dde90a5014ffb5853c123f438c6c62
robocoderbr <robocoderbr@gmail.com>
2021-12-26 21:11:04 +0300

    add order

--- order.js
+++ order.js
@@ -1,11 +1,11 @@
 
-const api_key = '78e1419893bbd454aca2ee6a329e7ed3f35fc';
-let url = 'https://products-94d2.restdb.io/rest/prykhodko-order';
+const api_key = "78e1419893bbd454aca2ee6a329e7ed3f35fc";
 
-function buyAll() { 
+  
+function buyAll() {
     cartMenu.innerHTML = 'Card is empty';
 
-    let data = JSON.stringify(localStorage.getItem('cart'));
+    var data = localStorage.getItem('cart')
       
       var xhr = new XMLHttpRequest();
       xhr.withCredentials = false;
@@ -15,13 +15,17 @@ function buyAll() {
           console.log(this.responseText);
         }
       });
-      xhr.open("POST", url);
+      
+      xhr.open("POST", "https://products-94d2.restdb.io/rest/pilipchuk-order");
       xhr.setRequestHeader("content-type", "application/json");
       xhr.setRequestHeader("x-apikey", api_key);
       xhr.setRequestHeader("cache-control", "no-cache");
       
       xhr.send(data);
-      
 
       localStorage.clear();
-    }
+
+}
+
+
+
