6f605846e322e28490a44668a59b300da40ee8ff
robocodebr <robocodebr@gmail.com>
2021-12-26 21:11:09 +0300

    add order

--- order.js
+++ order.js
@@ -1,16 +1,27 @@
-const api_key = '78e1419893bbd454aca2ee6a329e7ed3f35fc';
-function buyAll() { 
-    localStorage.clear();
-    cartMenu.innerHTML = 'Card is empty';
-    let json = [{}];
-    let collection = "masokha-order"
-    xhr.open("POST","https://products-94d2.restdb.io/rest/masokha-order/")
-        
-            "content-type": "application/json",
-            "x-apikey": api_key,
-            "cache-control": "no-cache"
-        }),
-        body: JSON.stringify(json)
-    }
-}
+const api_key = "78e1419893bbd454aca2ee6a329e7ed3f35fc";
 
+  
+function buyAll() { 
+    cartMenu.innerHTML = 'Card is empty';
+
+    var data = localStorage.getItem('cart')
+      
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = false;
+      
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
+        }
+      });
+      
+      xhr.open("POST", "https://products-94d2.restdb.io/rest/masokha-order");
+      xhr.setRequestHeader("content-type", "application/json");
+      xhr.setRequestHeader("x-apikey", api_key);
+      xhr.setRequestHeader("cache-control", "no-cache");
+      
+      xhr.send(data);
+
+      localStorage.clear();
+
+}
