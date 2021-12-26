1c355abe14fa0d7c8a3f52d7b704f9a1d4f717e8
robocodebr <robocedebr@gmail.com>
2021-12-26 20:11:57 +0300

    add order

--- order.js
+++ order.js
@@ -1,27 +1,28 @@
-const api_key = "78e1419893bbd454aca2ee6a329e7ed3f35fc";
 
-function buyAll() { 
-    cartMenu.innerHTML = 'Card is empty';
+      const api_key = "78e1419893bbd454aca2ee6a329e7ed3f35fc";
 
-    let data = JSON.stringify(localStorage.getItem('cart'));
+  
+      function buyAll() { 
+          cartMenu.innerHTML = 'Card is empty';
       
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = false;
+          var data = localStorage.getItem('cart')
+            
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = false;
+            
+            xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === 4) {
+                console.log(this.responseText);
+              }
+            });
+            
+            xhr.open("POST", "https://products-94d2.restdb.io/rest/mira-order");
+            xhr.setRequestHeader("content-type", "application/json");
+            xhr.setRequestHeader("x-apikey", api_key);
+            xhr.setRequestHeader("cache-control", "no-cache");
+            
+            xhr.send(data);
       
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-          console.log(this.responseText);
-        }
-      });
-      let collection = "surname-order"
-      xhr.open("POST", "https://products-94d2.restdb.io/rest/"+collection);
-      xhr.setRequestHeader("content-type", "application/json");
-      xhr.setRequestHeader("x-apikey", api_key);
-      xhr.setRequestHeader("cache-control", "no-cache");
+            localStorage.clear();
       
-      xhr.send(data);
-      
-
-      localStorage.clear();
-
-}
+      }
